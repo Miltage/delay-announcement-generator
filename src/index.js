84a55d09faeb25203cecbe7b779aa44f7319a8d8
Robert Shenton <theshenton@gmail.com>
2022-04-02 13:41:03 +0200

    Randomize on load

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.scss";
 
 console.log("Started!");
+
+var optionsIntro = ["To our valued customers,", "Greetings gamers!", "Dear friends,", "Hello everybody,"];
+
+var generated, introSpan;
+
+function randomize() {
+  introSpan.innerHTML = optionsIntro[Math.floor(Math.random() * optionsIntro.length)];
+}
+
+window.onload = function () {
+  generated = document.getElementById("generated");
+  introSpan = document.getElementById("intro");
+  randomize();
+  generated.classList.add("visible");
+}
